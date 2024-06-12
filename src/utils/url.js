6c506143fdde711c8fd7a503fc975e422a7417c6
Codeur16 <charlesnjiosseu@gmail.com>
2024-06-12 08:13:06 +0100

    final

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,7 @@
-export default url = "http://192.168.43.245:8080";
+//= "http://192.168.43.245:8080";
 //android:usesCleartextTraffic="true"
+
+ export default url =
+   process.env.NODE_ENV === "development"
+     ? "http://192.168.43.245:8080" // Adresse IP pour le développement local
+     : "https://yourproductiondomain.com/api/test";  // Adresse de production
